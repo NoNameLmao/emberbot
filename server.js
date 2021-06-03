30d2169c0f37ca172cce2d5c31b3dfd84bbb540a
NoNameLmao <egor5553049@gmail.com>
2021-06-03 10:31:02 +0300

    http server

--- server.js
+++ server.js
@@ -33,6 +33,54 @@ function updateMonth() {
     else europesimCurrentMonth = `December`
 };
 
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (request, response) {
+
+   console.log('request starting for ');
+   console.log(request);
+
+   var filePath = '.' + request.url;
+   if (filePath == './')
+       filePath = './index.html';
+
+   console.log(filePath);
+   var extname = path.extname(filePath);
+   var contentType = 'text/html';
+   switch (extname) {
+       case '.js':
+           contentType = 'text/javascript';
+           break;
+       case '.css':
+           contentType = 'text/css';
+           break;
+   }
+
+   path.exists(filePath, function(exists) {
+
+       if (exists) {
+           fs.readFile(filePath, function(error, content) {
+               if (error) {
+                   response.writeHead(500);
+                   response.end();
+               }
+               else {
+                   response.writeHead(200, { 'Content-Type': contentType });
+                   response.end(content, 'utf-8');
+               }
+           });
+       }
+       else {
+           response.writeHead(404);
+           response.end();
+       }
+   });
+
+}).listen(5000);
+
+console.log('Server running at http://127.0.0.1:5000/');
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 server.listen(server_port, server_host, function() {
