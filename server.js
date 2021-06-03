e33145a325df85daf8fedc0c59be1bfca10f45ea
NoNameLmao <egor5553049@gmail.com>
2021-06-03 14:00:49 +0300

    created config.json, changed token location from server.js to config.json

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require('./config.json');
 const disbut = require('discord-buttons')(client);
 const guildID = (`846807940727570433`); // 846807940727570433
 const botchannelID = (`846811100338323497`); // 846811100338323497
@@ -412,4 +413,4 @@ let helpEmbed = {
     ]
 };
 
-client.login(token);
+client.login(config.token);
