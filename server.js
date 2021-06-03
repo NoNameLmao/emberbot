cfce8d21d0c662e6425a3f30dfecae946a78886d
NoNameLmao <egor5553049@gmail.com>
2021-06-03 10:35:17 +0300

    testtesting

--- server.js
+++ server.js
@@ -78,14 +78,9 @@ http.createServer(function (request, response) {
        }
    });
 
-}).listen(5000);
+}).listen(process.env.PORT || 5000);
 
 console.log('Server running at http://127.0.0.1:5000/');
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
