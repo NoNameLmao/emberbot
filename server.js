9a609525f1f0322f1820bb1e1596cdd33c49e17d
NoNameLmao <egor5553049@gmail.com>
2021-06-03 10:24:55 +0300

    h7

--- server.js
+++ server.js
@@ -33,6 +33,12 @@ function updateMonth() {
     else europesimCurrentMonth = `December`
 };
 
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 };
