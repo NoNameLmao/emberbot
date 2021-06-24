2a90dea830cb37ff289f695049014b8cbfce6a30
NoNameLmao <egor5553049@gmail.com>
2021-06-24 18:20:02 +0300

    optimising the bot

--- server.js
+++ server.js
@@ -103,51 +103,51 @@ function getRandomInt(max) {
 };
 
 const TechnobladeQuote = [
-    {quote: 'NOT EVEN CLOSE BABY TECHNOBLADE NEVER DIES'}, // 1
-    {quote: 'technoblade never dies'}, // 2
-    {quote: 'dude these orphans are getting destroyed'}, // 3
-    {quote: 'this is the second worst thing that has happened to these orphans in their lives'}, // 4
-    {quote: 'subscribe to technoblade'}, // 5
-    {quote: 'if you wish to defeat me, train for another 100 years'}, // 6
-    {quote: 'did you try getting good?'}, // 7
-    {quote: 'i told you man... i am an anime protagonist'}, // 8
-    {quote: 'sometimes its tough being the best'}, // 9
-    {quote: 'blood for the blood god'}, // 10
-    {quote: 'im not saying im winning this game.. but... im winning this game'}, // 11
-    {quote: 'all part of my master plan'}, // 12
-    {quote: 'this is my main game'}, // 13
-    {quote: 'nerd spotted'}, // 14
-    {quote: 'i can tryhard any game!'}, // 15
-    {quote: 'im so good at video games'}, // 16
-    {quote: 'aaaaaand not even close'}, // 17
-    {quote: 'what a scam'}, // 18
-    {quote: 'thank you hypixel'}, // 19
-    {quote: 'dying is for casuals, forget what i did like 3 seconds ago'}, // 20
-    {quote: 'i play minecraft! dont tell my parents they think i have a job'}, // 21
-    {quote: 'i had no expectations and i still managed to get dissapointed... welcome to bedwars'}, // 22
-    {quote: 'lets cyberbully some nerds'}, // 23
-    {quote: 'im so good at this game'}, // 24
-    {quote: 'off the map you all go'}, // 25
-    {quote: 'weeeee'}, // 26
-    {quote: 'he tried...'}, // 27
-    {quote: 'sometimes i dream about trees'}, // 28
-    {quote: 'tommy, just... just stop talking'}, // 29
-    {quote: 'i call dibs on the planet'}, // 30
-    {quote: 'i just threw some guy off a ledge. with my bare fist!'}, // 31
-    {quote: 'weapons are for casuals'}, // 32
-    {quote: 'the cyberbullying is off to a good start'}, // 33
-    {quote: 'a little known fact, im actually the best fortnite player of all time'}, // 34
-    {quote: 'am i wearing pants right now? you just have to take my word for it'}, // 35
-    {quote: 'cant run away from your problems when they have ender pearls'} // 36
+    'NOT EVEN CLOSE BABY TECHNOBLADE NEVER DIES',
+    'technoblade never dies',
+    'dude these orphans are getting destroyed',
+    'this is the second worst thing that has happened to these orphans in their lives',
+    'subscribe to technoblade',
+    'if you wish to defeat me, train for another 100 years',
+    'did you try getting good?',
+    'i told you man... i am an anime protagonist',
+    'sometimes its tough being the best',
+    'blood for the blood god',
+    'im not saying im winning this game.. but... im winning this game',
+    'all part of my master plan',
+    'this is my main game',
+    'nerd spotted',
+    'i can tryhard any game!',
+    'im so good at video games',
+    'aaaaaand not even close',
+    'what a scam',
+    'thank you hypixel',
+    'dying is for casuals, forget what i did like 3 seconds ago',
+    'i play minecraft! dont tell my parents they think i have a job',
+    'i had no expectations and i still managed to get dissapointed... welcome to bedwars',
+    'lets cyberbully some nerds',
+    'im so good at this game',
+    'off the map you all go',
+    'weeeee',
+    'he tried...',
+    'sometimes i dream about trees',
+    'tommy, just... just stop talking',
+    'i call dibs on the planet',
+    'i just threw some guy off a ledge. with my bare fist!',
+    'weapons are for casuals',
+    'the cyberbullying is off to a good start',
+    'a little known fact, im actually the best fortnite player of all time',
+    'am i wearing pants right now? you just have to take my word for it',
+    'cant run away from your problems when they have ender pearls'
 ];
 let quoteInt = getRandomInt(TechnobladeQuote.length + 1);
 
 const liechtenstein = [
-    {misspell: 'lichestien'},
-    {misspell: 'lichistint'},
-    {misspell: 'lichtenstein'},
-    {misspell: 'liehctenstein'},
-    {misspell: 'liechtenstien'}
+    'lichestien',
+    'lichistint',
+    'lichtenstein',
+    'liehctenstein',
+    'liechtenstien'
 ]
 
 function sleep(ms) {
@@ -243,11 +243,11 @@ client.on('ready', async() => {
           ]
         };
         let helpEmbed = {
-            "title": "All list of commands (Main branch)",
-            "description": `prefix: .`,
+            "title": "All list of commands",
+            "description": `prefix: ${config.prefix}`,
             "color": 53380,
             "footer": {
-                "text": "Added random number generator!"
+                "text": "hey maybe its a great thing that the entire bot is in one file lol idc that much"
             },
             "fields": [
                 {
@@ -272,12 +272,12 @@ client.on('ready', async() => {
                 },
                 {
                   "name": "quote",
-                  "value": `${TechnobladeQuote[quoteInt].quote} (random technoblade quote)`,
+                  "value": `${TechnobladeQuote[quoteInt]} (random technoblade quote)`,
                   "inline": true
                 },
                 {
                     "name": "suggest",
-                    "value": "Suggest some ideas, might add it to the bot",
+                    "value": "Suggest some ideas, might add them to the bot",
                     "inline": true
                 },
                 {
@@ -286,8 +286,8 @@ client.on('ready', async() => {
                     "inline": true
                 },
                 {
-                    "name": "rng (minNumber) (maxNumber)",
-                    "value": "Random number generator (min value is optional)",
+                    "name": "rng <minValue> (maxValue)",
+                    "value": "Random number generator (minValue is optional)",
                     "inline": true
                 },
                 {
@@ -336,7 +336,7 @@ client.on('ready', async() => {
                     message.channel.send(`\`Code ran with an error:\` \`\`\`xl\n${error}\n\`\`\``);
                     console.log(`recieved ${command} command from ${message.author.tag} @ ${now.toString()} ${message.content} \n${code} \nThere was an error running this code: \n${error}`);
                 };
-            } else return channel.send(`${TechnobladeQuote[quoteInt].quote} (No permission)`);
+            } else return channel.send(`${TechnobladeQuote[quoteInt]} (No permission)`);
         }
         else if (command === "exit") {
             if (message.author.id === `341123308844220447`) {
@@ -346,7 +346,7 @@ client.on('ready', async() => {
             else {
                 console.log(`recieved exit command from ${message.author.tag} @ ${now.toString()} lol permission denied have a technoblade quote instead nerd`)
                 let quoteInt = getRandomInt(37);
-                channel.send(TechnobladeQuote[quoteInt].quote);    
+                channel.send(TechnobladeQuote[quoteInt]);    
             } return;
         }
         else if (command === "sudo") {
@@ -368,11 +368,11 @@ client.on('ready', async() => {
                 const sudo = args.join(" ");
                 message.channel.send(sudo);
             }
-            else return channel.send(`${TechnobladeQuote[quoteInt].quote} (No permission)`);
+            else return channel.send(`${TechnobladeQuote[quoteInt]} (No permission)`);
         }  
         else if (command === "quote") {
             quoteInt = getRandomInt(37);
-            return message.channel.send(TechnobladeQuote[quoteInt].quote);
+            return message.channel.send(TechnobladeQuote[quoteInt]);
         }
         else if (command === "suggest") {
             const suggest = args.join(" ");
