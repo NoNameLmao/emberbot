11e21fbc35cd74d8dbbb262361013c9cc2d5d1a3
NoNameLmao <egor5553049@gmail.com>
2021-06-25 22:35:10 +0300

    cat pic owo

--- server.js
+++ server.js
@@ -151,7 +151,8 @@ const hiMessage = [
     'https://media.discordapp.net/attachments/801529722513129543/801564698763067402/caption-2.gif',
     'https://cdn.discordapp.com/attachments/655219751739457547/802619982755528704/image0_5.gif',
     'https://tenor.com/view/get-real-cat-skate-funny-meme-gif-18666878',
-    'https://media.discordapp.net/attachments/512345897577742339/856844900564598804/Screenshot_20210620-020741.png?width=534&height=537'
+    'https://media.discordapp.net/attachments/512345897577742339/856844900564598804/Screenshot_20210620-020741.png?width=534&height=537',
+    'https://cdn.discordapp.com/attachments/512345897577742339/852661473020215376/FB_IMG_16233607158116289.jpg'
 ];
 let hiMsgInt = getRandomInt(hiMessage.length + 1);
 
