89f32f05fe4703234f03e4fc8e2c41525a52bbc2
NoNameLmao <68788282+NoNameLmao@users.noreply.github.com>
2021-06-26 03:10:44 +0300

    sinus

--- server.js
+++ server.js
@@ -154,7 +154,8 @@ const hiMessage = [
     'https://tenor.com/view/get-real-cat-skate-funny-meme-gif-18666878',
     'https://media.discordapp.net/attachments/512345897577742339/856844900564598804/Screenshot_20210620-020741.png?width=534&height=537',
     'https://cdn.discordapp.com/attachments/512345897577742339/852661473020215376/FB_IMG_16233607158116289.jpg',
-    'https://cdn.discordapp.com/attachments/653987873548271616/828690882092990524/unknown.png'
+    'https://cdn.discordapp.com/attachments/653987873548271616/828690882092990524/unknown.png',
+    'https://cdn.discordapp.com/attachments/603303568317087798/858130347581767700/image0-2.png'
 ];
 let hiMsgInt = getRandomInt(hiMessage.length + 1);
 
