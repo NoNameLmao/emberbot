1b844a3adc8e85b05a217414926f815739524d89
NoNameLmao <egor5553049@gmail.com>
2021-07-29 18:35:24 +0300

    random code changes please work

--- server.js
+++ server.js
@@ -212,9 +212,9 @@ client.on('ready', async() => {
     client.on('error', error => console.log(error));
     client.on('message', function(message) {
         if (message.guild.id === "746145375169282160" && message.channel.id === "870017944380403772") {
-            message.content.replace(/@(?=everyone|here)/g, "@\u200b")
             if (message.author.bot) return;
             else try {
+                message.content.replace(/@(?=everyone|here)/g, "@\u200b")
                 let msg = new webhook.MessageBuilder().setName(message.author.username).setText(message.content.toString()).setAvatar(message.author.avatarURL())
                 europesimHook.send(msg);
                 message.react('✅');
@@ -227,6 +227,7 @@ client.on('ready', async() => {
             message.content.replaceAll("@everyone", "@\u200beveryone").replaceAll("@here", "@\u200bhere");
             if (message.author.bot) return;
             else try {
+                message.content.replace(/@(?=everyone|here)/g, "@\u200b")
                 let msg = new webhook.MessageBuilder().setName(message.author.username).setText(message.content.toString()).setAvatar(message.author.avatarURL()) 
                 frozenworldHook.send(msg);
                 message.react('✅');
