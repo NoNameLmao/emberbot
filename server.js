14408e537f7d423f57d17ee3e3e3adc6d3d07c87
NoNameLmao <egor5553049@gmail.com>
2021-10-10 18:49:08 +0300

    chatbot now has dots again, owner set

--- server.js
+++ server.js
@@ -216,10 +216,9 @@ client.on('ready', async () => {
                 else {
                     if (!message.content) return message.react('❌');
                     message.channel.sendTyping();
-                    scb.chat({ message: message.content, name: client.user.username, user: message.author.id, language: "auto" }).then(msg => {
+                    scb.chat({ message: message.content, name: client.user.username, user: message.author.id, owner: "emberglaze", language: "auto" }).then(msg => {
                         message.reply({
                             content: msg.toLowerCase()
-                            .replace('.', '')
                             .replace(`'`, '')
                             .replace('you can interrupt me at any time by clicking the “x” on the top-right', 'if you want me to stop then stop talking here, its that simple')
                             .replace('ok, ill stop when you click the “x” on the top-right', 'i will stop once you stop typing here dude'),
