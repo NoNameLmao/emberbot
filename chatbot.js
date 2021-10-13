ffec10c9784e095bf5c247dcbe90a7e1c80a896f
NoNameLmao <egor5553049@gmail.com>
2021-10-13 22:32:47 +0300

    chatbot addition #2

--- chatbot.js
+++ chatbot.js
@@ -7,9 +7,21 @@ net.train([
     { input: 'hello', output: 'hi' },
     { input: 'test', output: 'just a test.' },
     { input: 'how are you', output: 'im good' },
+    { input: 'search joe', output: 'joe who sir' },
     { input: 'this is better', output: 'sound good' },
+    { input: 'he big brain', output: 'i big brain yes'},
+    { input: 'my name is deez', output: 'deez what sir' },
+    { input: 'are you technoblade', output: 'not yet sadly' },
     { input: 'pls talk', output: 'i dont talk, i only send messages' },
-]);
+    { input: 'are you better than the older one', output: 'not yet, if you want an already polished company then .chatbot' },
+], {
+    iterations: 2000,
+    log: true,
+    errorThresh: 0.001,
+    logPeriod: 50,
+    momentum: 0.1,
+    learningRate: 0.001,
+});
 
 /**
  * Run a string through the net.
