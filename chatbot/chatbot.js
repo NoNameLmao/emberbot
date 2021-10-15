20ca11c85aa964642d86414f7f3254d25b47660d
NoNameLmao <egor5553049@gmail.com>
2021-10-15 21:29:11 +0300

    [Chatbot] removed neutralnet.json check

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -3,11 +3,7 @@ const brain = require('brain.js');
 const net = new brain.recurrent.LSTM();
 let { botChannel } = require('../server');
 
-if (fs.existsSync('neuralnet.json')) {
-    net.fromJSON(JSON.parse(fs.readFileSync('neuralnet.json', 'utf8')));
-} else {
-    botChannel.send(':x: `Can\'t find the neuralnet file, did you run the train.js thing lol`');
-}
+net.fromJSON(JSON.parse(fs.readFileSync('./neuralnet.json', 'utf8')));
 
 /**
  * Run a string through the net.
