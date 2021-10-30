837cecbda11650487c23f7dd9d82b32ca042c46c
NoNameLmao <egor5553049@gmail.com>
2021-10-30 22:14:12 +0200

    just resolve jsons bruh

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   apps : [{
     name: 'esbot',
-    script: 'server.js',
+    script: 'index.ts',
     watch: 'true',
     env_hook: {
       command: 'git pull && npm i && npm test && pm2 restart esbot'
-    }
+    },
+    interpreter: '/root/.nvm/versions/node/v16.9.1/bin/ts-node',
+    interpreter_args: '--resolveJsonModule'
   }]
 };
