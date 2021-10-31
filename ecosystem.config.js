27a8a4f4d672d9106bd24076bebca01c723b5008
NoNameLmao <egor5553049@gmail.com>
2021-10-31 18:53:42 +0100

    put ts-node in script duh

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,10 @@
 module.exports = {
   apps : [{
     name: 'esbot',
-    script: 'index.ts',
+    script: 'ts-node index.ts',
     watch: 'true',
     env_hook: {
       command: 'git pull && npm i && npm test && pm2 restart esbot'
     },
-    interpreter: '/root/.nvm/versions/node/v16.9.1/bin/ts-node',
-    interpreter_args: '--resolveJsonModule'
   }]
 };
