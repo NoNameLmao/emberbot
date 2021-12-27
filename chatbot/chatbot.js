9407ebb2c914009d51bef1a4791d2ad440f97a67
NoNameLmao <egor5553049@gmail.com>
2021-12-27 16:13:09 +0300

    remove unused require botChannel

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const brain = require('brain.js');
 const net = new brain.recurrent.LSTM();
-let { botChannel } = require('../index.ts');
 
 net.fromJSON(JSON.parse(fs.readFileSync('./chatbot/neuralnet.json', 'utf8')));
 
