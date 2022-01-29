ef6bcc37fa7c3b7ebd5073841066ec7092985087
NoNameLmao <egor5553049@gmail.com>
2022-01-29 13:26:07 +0300

    ping pong

--- commands/ping.ts
+++ commands/ping.ts
@@ -6,8 +6,8 @@ module.exports = {
     description: 'Get discord latency (compares current time to the time when the command was sent)',
     async run(message: Message) {
         message.channel.send(
-            `Message-measured ping: ${Date.now() - message.createdTimestamp}ms
-            WebSocket ping: ${message.client.ws.ping}ms`
+            `Message-measured ping: **${Date.now() - message.createdTimestamp}**ms\n` +
+            `WebSocket ping: **${message.client.ws.ping}**ms`
         )
     }
 } as Command;
